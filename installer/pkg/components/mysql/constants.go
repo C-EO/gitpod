c2c8cdbe3bcd3d87c51fc399f0622d09142183ea
Simon Emms <simon@gitpod.io>
2021-10-05 13:11:00 +0100

    [installer]: allow importing of charts from Helm

---
+++ installer/pkg/components/mysql/constants.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package mysql
+
+const (
+	Component       = "mysql"
+	SQLInitScripts  = "db-init-scripts"
+	SQLPasswordName = "db-password"
+)
---
+++ installer/pkg/components/rabbitmq/constants.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package rabbitmq
+
+const (
+	Component    = "rabbitmq"
+	CookieSecret = "messagebus-erlang-cookie"
+	TLSSecret    = "messagebus-certificates-secret-core"
+)
